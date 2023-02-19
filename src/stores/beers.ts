4a5b194645b526498dcaffb1ba3eef68c49c55cd
Zgon <karim.mannai7@gmail.com>
2023-02-19 21:33:42 +0100

    create beer store

---
+++ src/stores/beers.ts
@@ -0,0 +1,28 @@
+import { defineStore } from 'pinia';
+import type { Beer } from '@/models/beer.model';
+import axios from 'axios';
+
+const URL = import.meta.env.VITE_PUNKAPI_URL;
+
+export const useBeersStore = defineStore('beers', {
+  state: () => ({
+    beers: [] as Beer[],
+    loading: false,
+    error: null as Error | null
+  }),
+
+  actions: {
+    async fetchBeers(): Promise<any> {
+      this.loading = true;
+      try {
+        console.log('fetching beers');
+        const response = await axios.get(URL + '?per_page=10');
+        this.beers = response.data;
+      } catch (error) {
+        this.error = error as Error;
+      } finally {
+        this.loading = false;
+      }
+    }
+  }
+});
