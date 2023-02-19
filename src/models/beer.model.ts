75358934d923ad2c9f53d7235159220d021c85f7
Zgon <karim.mannai7@gmail.com>
2023-02-19 21:32:18 +0100

    create Beer model

---
+++ src/models/beer.model.ts
@@ -0,0 +1,16 @@
+export interface Beer {
+  id: number;
+  name: string;
+  tagline: string;
+  first_brewed: string;
+  description: string;
+  image_url: string;
+  abv: number;
+  ibu: number;
+  target_fg: number;
+  target_og: number;
+  ebc: number;
+  srm: number;
+  ph: number;
+  attenuation_level: number;
+}
