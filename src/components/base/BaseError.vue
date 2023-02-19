c9c6ea79de690264a607be63e3fc43bfac32ae15
Zgon <karim.mannai7@gmail.com>
2023-02-19 21:47:33 +0100

    implement BaseError component

---
+++ src/components/base/BaseError.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="lg:gap-4 lg:flex">
+      <div class="flex flex-col items-center justify-center md:py-24 lg:py-32">
+        <h1 class="font-bold text-blue-600 text-9xl">Oops!</h1>
+        <p class="mt-5 text-2xl font-bold text-center text-gray-800 md:text-3xl">An error occurred</p>
+      </div>
+      <img src="src/assets/beer.jpg" alt="img" class="object-cover w-full h-full" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
