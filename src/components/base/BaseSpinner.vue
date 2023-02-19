9b6ba4cc20ccd1a24583b9e3e402903fdaf47f93
Zgon <karim.mannai7@gmail.com>
2023-02-19 21:36:21 +0100

    implement BaseSpinner component

---
+++ src/components/base/BaseSpinner.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <div class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  console.log('BaseSpinner mounted');
+});
+</script>
