75cbdc43a9766d75a68012e5ab523c8c3d839d8f
Zgon <karim.mannai7@gmail.com>
2023-02-24 00:59:52 +0100

    create BaseInput component

---
+++ src/components/base/BaseInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <label :for="label" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ label }}</label>
+    <input
+      :type="type"
+      :name="label"
+      :id="label"
+      class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      :placeholder="placeholder"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ label: string; type: string; placeholder: string }>();
+</script>
+
+<style scoped></style>
