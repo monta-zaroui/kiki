afa789132b63af989dd60fdc335bd4c95d8dd105
Zgon <karim.mannai7@gmail.com>
2023-02-24 00:59:32 +0100

    create baseLogo component

---
+++ src/components/base/BaseLogo.vue
@@ -0,0 +1,10 @@
+<template>
+  <span class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+    <img class="w-8 h-8 mr-2" src="src/assets/logo.png" alt="logo" />
+    Avantgarde/Slash Assignment
+  </span>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
