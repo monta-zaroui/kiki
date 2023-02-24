1b6c7ab457636ba5d65f3d900c0607ba715ab4c6
Zgon <karim.mannai7@gmail.com>
2023-02-24 14:53:41 +0100

    #BE create user model

---
+++ ava-be-challenge/src/models/User.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  favoriteBeers: [
+    {
+      type: Number,
+      default: []
+    }
+  ]
+});
+
+/**
+ * @typedef User
+ */
+export default mongoose.model('User', userSchema);
