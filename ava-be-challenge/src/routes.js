aba95fa9ee4af1248ae018af7e96deab7d2cb7d9
Zgon <karim.mannai7@gmail.com>
2023-02-24 17:00:26 +0100

    #BE add healthcheck route

--- ava-be-challenge/src/routes.js
+++ ava-be-challenge/src/routes.js
@@ -3,6 +3,17 @@ import userRoutes from './modules/user/user.routes.js';
 
 const router = express.Router();
 
+/**
+ * GET /v2/base/healthcheck - Check service health
+ */
+router.get('/v2/base/healthcheck', (req, res) =>
+  res.json({
+    server_time: new Date().toISOString(),
+    timezone: new Date().toLocaleString('en-US', { timeZoneName: 'short' }).split(' ').pop(),
+    version: process.env.npm_package_version
+  })
+);
+
 router.use('/users', userRoutes);
 
 export default router;
