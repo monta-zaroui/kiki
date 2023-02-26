3f6c721025b679014124617735091a14daeb9080
Zgon <karim.mannai7@gmail.com>
2023-02-26 18:07:45 +0100

    #FE: list view displays logic

--- ava-fe-challenge/src/components/BeersOverview.vue
+++ ava-fe-challenge/src/components/BeersOverview.vue
@@ -8,17 +8,24 @@
   </template>
 
   <template v-else>
-    <div ref="scrollComponent">
-      <RouterLink to="/beer/11"> (test) Go to Beer 11</RouterLink>
-      <BaseCard v-for="beer in beers" :key="beer.id" :beer="beer" />
-      <BaseSpinner v-if="loadingMore" />
+    <div class="w-screen p-2 sm:p-4 md:p-6 lg:p-12 flex justify-center">
+      <div ref="scrollComponent" class="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-10">
+        <BaseCard
+          v-for="(beer, index) in beers"
+          :key="beer.id"
+          :beer="beer"
+          :index="index"
+          :is-image-on-left="isImageOnTheLeft(index)"
+          :screen-width="screenWidth"
+        />
+      </div>
     </div>
   </template>
 </template>
 
 <script setup lang="ts">
 import { onBeforeMount, onMounted, onUnmounted, ref } from 'vue';
-import { useBeersStore } from '../stores/beers';
+import { useBeersStore } from '@/stores/beers';
 import { storeToRefs } from 'pinia';
 import BaseSpinner from '@/components/base/BaseSpinner.vue';
 import BaseError from '@/components/base/BaseError.vue';
@@ -27,15 +34,18 @@ import BaseCard from '@/components/base/BaseCard.vue';
 const beersStore = useBeersStore();
 const { beers, loading, loadingMore, error } = storeToRefs(beersStore);
 const scrollComponent = ref(null);
+const screenWidth = ref<number>(window.innerWidth);
 
 onBeforeMount(() => beersStore.fetchBeers());
 
 onMounted(() => {
   window.addEventListener('scroll', handleInfiniteScroll);
+  window.addEventListener('resize', handleResize);
 });
 
 onUnmounted(() => {
   window.removeEventListener('scroll', handleInfiniteScroll);
+  window.removeEventListener('resize', handleResize);
 });
 
 function onLoadMore() {
@@ -44,12 +54,17 @@ function onLoadMore() {
   beersStore.fetchBeers();
 }
 
+const handleResize = () => {
+  screenWidth.value = window.innerWidth;
+};
+
 const handleInfiniteScroll = () => {
   const element: any = scrollComponent.value;
   if (element.getBoundingClientRect().bottom < window.innerHeight) {
     onLoadMore();
   }
 };
-</script>
 
+const isImageOnTheLeft = (index: number): boolean => index % 4 !== 2 && index % 4 !== 3;
+</script>
 <style scoped></style>
--- ava-fe-challenge/src/components/base/BaseCard.vue
+++ ava-fe-challenge/src/components/base/BaseCard.vue
@@ -1,30 +1,47 @@
 <template>
   <div class="flex">
-    <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
-      <img
-        class="w-125 h-20 md:h-auto object-cover md:w-20 rounded-t-lg md:rounded-none md:rounded-l-lg"
-        :src="image_url"
-        alt="Image of beer"
-      />
-      <div class="p-6 flex flex-col justify-start">
+    <div class="flex rounded-lg shadow-lg w-full p-6 h-80">
+      <img v-if="displayImageOnLeft" class="h-full w-28 rounded-lg" :src="image_url" :alt="beer.name" />
+      <div class="p-4 flex flex-col justify-start">
         <h5 class="text-gray-900 text-xl font-medium mb-2">{{ beer.name }}</h5>
-        <p class="text-gray-700 text-base mb-4">
+        <p class="text-gray-700 text-base mb-4 line-clamp-3">
           {{ beer.description }}
         </p>
-        <p class="text-gray-600 text-xs">{{ beer.contributed_by }}</p>
+        <p class="text-gray-600 text-xs">{{ screenWidth }}</p>
       </div>
+      <img
+        v-if="!displayImageOnLeft && !iSSmallScreen"
+        class="w-28 h-full rounded-lg"
+        :src="image_url"
+        :alt="beer.name"
+      />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { Beer } from '@/models/beer.model';
+import { computed } from 'vue';
 
 const props = defineProps<{
   beer: Beer;
+  isImageOnLeft: boolean;
+  screenWidth: number;
+  index: number;
 }>();
+const image_url = props.beer.image_url || 'src/assets/beer.jpg';
 
-const image_url = props.beer.image_url || '/src/assets/beer.jpg';
+const displayImageOnLeft = computed((): boolean => {
+  if (iSSmallScreen.value) {
+    return true;
+  } else if (props.screenWidth >= 768 && props.isImageOnLeft) {
+    return true;
+  } else return props.screenWidth > 640 && props.screenWidth < 768 && props.index % 2 === 0;
+});
+
+const iSSmallScreen = computed((): boolean => {
+  return props.screenWidth <= 640;
+});
 </script>
 
 <style scoped></style>
