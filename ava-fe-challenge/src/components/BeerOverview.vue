b617806ba2098e63bb20c37d4b88faaa5d63ec6c
Zgon <karim.mannai7@gmail.com>
2023-02-26 21:48:47 +0100

    #FE: Designing BaseCard

--- ava-fe-challenge/src/components/BeerOverview.vue
+++ ava-fe-challenge/src/components/BeerOverview.vue
@@ -7,60 +7,52 @@
     <div class="container mx-auto px-4 py-8">
       <div class="flex flex-col md:flex-row justify-between items-center">
         <div class="w-full md:w-1/2">
-          <img :src="beer.image_url" :alt="beer.name" class="w-full mb-8">
-          <h1 class="text-3xl font-bold mb-2">{{ beer.name }}</h1>
-          <p class="text-gray-500 text-lg mb-4">{{ beer.tagline }}</p>
-          <p class="text-gray-600 mb-8">{{ beer.description }}</p>
-          <h2 class="text-2xl font-bold mb-2">Brewers Tips:</h2>
+          <img :src="beer.image_url" :alt="beer.name" class="h-full w-45 rounded-lg">
+          <p class="text-gray-500 text-lg mb-4">{{ beer.tagline }} {{beer.first_brewed}} By {{beer.contributed_by}}</p>
+          <h2 class="text-2xl font-bold mb-2">Tips:</h2>
           <p class="text-gray-600">{{ beer.brewers_tips }}</p>
         </div>
         <div class="w-full md:w-1/2">
-          <div class="bg-white p-4 rounded-lg shadow-md">
-            <div class="mb-4">
-              <h2 class="text-2xl font-bold mb-2">Beer Details:</h2>
-              <div class="grid grid-cols-2 gap-4">
-                <div class="text-gray-600 font-bold">ABV:</div>
-                <div class="text-gray-600">{{ beer.abv }}%</div>
-                <div class="text-gray-600 font-bold">IBU:</div>
-                <div class="text-gray-600">{{ beer.ibu }}</div>
-                <div class="text-gray-600 font-bold">EBC:</div>
-                <div class="text-gray-600">{{ beer.ebc }}</div>
-                <div class="text-gray-600 font-bold">SRM:</div>
-                <div class="text-gray-600">{{ beer.srm }}</div>
-                <div class="text-gray-600 font-bold">PH:</div>
-                <div class="text-gray-600">{{ beer.ph }}</div>
-              </div>
-            </div>
-            <div class="mb-4">
-              <h2 class="text-2xl font-bold mb-2">Ingredients:</h2>
-              <div class="grid grid-cols-2 gap-4">
-                <div class="text-gray-600 font-bold">Malt:</div>
-                <div>
-                  <ul class="list-disc list-inside">
-                    <li v-for="malt in beer.ingredients.malt" :key="malt.name">{{ malt.name }} - {{ malt.amount.value }} {{ malt.amount.unit }}</li>
+          <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+              <caption class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+                {{beer.name}}
+                <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">{{ beer.description }}</p>
+              </caption>
+              <caption class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+                <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400"><strong>ABV:</strong> {{ beer.abv }}% | <strong>Ph:</strong> {{ beer.ph }}% | <strong>Ibu:</strong> {{ beer.ibu }} <strong>Volume:</strong> {{ beer.volume.value}} {{beer.volume.unit}}</p>
+                <p>Ingredients</p>
+              </caption>
+              <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" class="px-6 py-3">
+                  Malt
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Hops
+                </th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  <ul>
+                    <li v-for="malt in beer.ingredients.malt" :key="malt.name">
+                      <strong>{{malt.name}}:</strong> {{malt.amount.value}} {{malt.amount.unit}}
+                    </li>
                   </ul>
-                </div>
-                <div class="text-gray-600 font-bold">Hops:</div>
-                <div>
-                  <ul class="list-disc list-inside">
-                    <li v-for="hop in beer.ingredients.hops" :key="hop.name">{{ hop.name }} - {{ hop.amount.value }} {{ hop.amount.unit }}, {{ hop.add }} {{ hop.attribute }}</li>
+                </th>
+                <td class="px-6 py-4">
+                  <ul>
+                    <li v-for="hops in beer.ingredients.hops" :key="hops.name">
+                      <strong>{{hops.name}}:</strong> {{hops.amount.value}} {{hops.amount.unit}} | {{hops.add}} | {{hops.attribute}}
+                    </li>
                   </ul>
-                </div>
-                <div class="text-gray-600 font-bold">Yeast:</div>
-                <div class="text-gray-600">{{ beer.ingredients.yeast }}</div>
-              </div>
-            </div>
-            <div>
-              <h2 class="text-2xl font-bold mb-2">Food Pairing:</h2>
-              <div class="grid grid-cols-2 gap-4">
-                <div class="text-gray-600 font-bold">Recommended Food:</div>
-                <div>
-                  <ul class="list-disc list-inside">
-                    <li v-for="food in beer.food_pairing" :key="food">{{ food }}</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
+                </td>
+              </tr>
+
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
--- ava-fe-challenge/src/components/base/BaseCard.vue
+++ ava-fe-challenge/src/components/base/BaseCard.vue
@@ -1,27 +1,29 @@
 <template>
   <div class="flex">
-    <div class="flex rounded-lg shadow-lg w-full p-6 h-80">
-      <img v-if="displayImageOnLeft" class="h-full w-28 rounded-lg" :src="image_url" :alt="beer.name" />
-      <div class="p-4 flex flex-col justify-start">
-        <h5 class="text-gray-900 text-xl font-medium mb-2">{{ beer.name }}</h5>
-        <StarIconSolid
-          v-if="isFavorite(beer.id)"
-          class="h-5 w-5 text-yellow-400 cursor-pointer"
-          @click="setFavorites('remove')"
+    <RouterLink :to="'/beer/' + beer.id">
+      <div class="flex rounded-lg shadow-lg w-full p-6 h-80">
+        <img v-if="displayImageOnLeft" class="h-full w-28 rounded-lg" :src="image_url" :alt="beer.name" />
+        <div class="p-4 flex flex-col justify-start">
+          <h5 class="text-gray-900 text-xl font-medium mb-2">{{ beer.name }}</h5>
+          <StarIconSolid
+            v-if="isFavorite(beer.id)"
+            class="h-5 w-5 text-yellow-400 cursor-pointer"
+            @click="setFavorites('remove')"
+          />
+          <StarIconOutline v-else class="h-5 w-5 text-yellow-400 cursor-pointer" @click="setFavorites('add')" />
+          <p class="text-gray-700 text-base mb-4 line-clamp-3">
+            {{ beer.description }}
+          </p>
+          <p class="text-gray-600 text-xs">{{ screenWidth }}</p>
+        </div>
+        <img
+          v-if="!displayImageOnLeft && !iSSmallScreen"
+          class="w-28 h-full rounded-lg"
+          :src="image_url"
+          :alt="beer.name"
         />
-        <StarIconOutline v-else class="h-5 w-5 text-yellow-400 cursor-pointer" @click="setFavorites('add')" />
-        <p class="text-gray-700 text-base mb-4 line-clamp-3">
-          {{ beer.description }}
-        </p>
-        <p class="text-gray-600 text-xs">{{ screenWidth }}</p>
       </div>
-      <img
-        v-if="!displayImageOnLeft && !iSSmallScreen"
-        class="w-28 h-full rounded-lg"
-        :src="image_url"
-        :alt="beer.name"
-      />
-    </div>
+    </RouterLink>
   </div>
 </template>
 
