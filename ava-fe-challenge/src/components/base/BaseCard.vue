2c8df06ecbb0d283146c987cee16d5d5cbaaa2b8
Zgon <karim.mannai7@gmail.com>
2023-02-26 18:53:22 +0100

    #FE: Add/remove favorites

--- ava-fe-challenge/src/components/base/BaseCard.vue
+++ ava-fe-challenge/src/components/base/BaseCard.vue
@@ -4,6 +4,12 @@
       <img v-if="displayImageOnLeft" class="h-full w-28 rounded-lg" :src="image_url" :alt="beer.name" />
       <div class="p-4 flex flex-col justify-start">
         <h5 class="text-gray-900 text-xl font-medium mb-2">{{ beer.name }}</h5>
+        <StarIconSolid
+          v-if="isFavorite(beer.id)"
+          class="h-5 w-5 text-yellow-400"
+          @click="setFavorites(beer.id, 'remove')"
+        />
+        <StarIconOutline v-else class="h-5 w-5 text-yellow-400" @click="setFavorites(beer.id, 'add')" />
         <p class="text-gray-700 text-base mb-4 line-clamp-3">
           {{ beer.description }}
         </p>
@@ -22,6 +28,9 @@
 <script setup lang="ts">
 import type { Beer } from '@/models/beer.model';
 import { computed } from 'vue';
+import { StarIcon as StarIconSolid } from '@heroicons/vue/24/solid';
+import { StarIcon as StarIconOutline } from '@heroicons/vue/24/outline';
+import { useAuthStore } from '@/stores/auth';
 
 const props = defineProps<{
   beer: Beer;
@@ -29,6 +38,8 @@ const props = defineProps<{
   screenWidth: number;
   index: number;
 }>();
+
+const authStore = useAuthStore();
 const image_url = props.beer.image_url || 'src/assets/beer.jpg';
 
 const displayImageOnLeft = computed((): boolean => {
@@ -42,6 +53,12 @@ const displayImageOnLeft = computed((): boolean => {
 const iSSmallScreen = computed((): boolean => {
   return props.screenWidth <= 640;
 });
+
+const isFavorite = (beerId: number) => authStore.user!.favoriteBeers.includes(beerId);
+
+const setFavorites = async (beerId: number, action: string) => {
+  await authStore.updateFavoriteBeers(beerId, action);
+};
 </script>
 
 <style scoped></style>
